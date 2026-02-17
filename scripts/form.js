b84ba337086af0b4c01e5d3e4e2935ee611248f4
Danson Nganga <dansonnganga019@gmail.com>
2026-02-17 14:56:35 +0300

    commit

--- scripts/form.js
+++ scripts/form.js
@@ -1,30 +1,45 @@
 const yearSpan = document.querySelector('#year');
 const lastModifiedSpan = document.querySelector('#lastModified');
 
-yearSpan.textContent = new Date().getFullYear();
-lastModifiedSpan.textContent = document.lastModified;
+if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+}
+
+if (lastModifiedSpan) {
+    lastModifiedSpan.textContent = document.lastModified;
+}
+
 
 const products = [
-    { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
-    { id: "fc-2050", name: "power laces", averagerating: 4.7 },
-    { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
-    { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
-    { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+    { id: "fc-1888", name: "Flux Capacitor", averagerating: 4.5 },
+    { id: "fc-2050", name: "Power Laces", averagerating: 4.7 },
+    { id: "fs-1987", name: "Time Circuits", averagerating: 3.5 },
+    { id: "ac-2000", name: "Low Voltage Reactor", averagerating: 3.9 },
+    { id: "jj-1969", name: "Warp Equalizer", averagerating: 5.0 }
 ];
 
 const productSelect = document.getElementById("product");
-products.forEach(product => {
-    const option = document.createElement("option");
-    option.value = product.id;
-    option.textContent = product.name;
-    productSelect.appendChild(option);
-});
+
+if (productSelect) {
+    products.forEach(product => {
+        const option = document.createElement("option");
+        option.value = product.id;
+        option.textContent = `${product.name} (${product.averagerating}★)`;
+        productSelect.appendChild(option);
+    });
+}
+
 
 if (window.location.pathname.endsWith("review.html")) {
+
     let counter = localStorage.getItem("reviewCount") || 0;
     counter = parseInt(counter) + 1;
     localStorage.setItem("reviewCount", counter);
-    const msg = document.createElement("p");
-    msg.textContent = `Total reviews submitted: ${counter}`;
-    document.body.appendChild(msg);
+
+    const counterDiv = document.getElementById("reviewCounter");
+
+    if (counterDiv) {
+        counterDiv.textContent = `Total reviews submitted: ${counter}`;
+    }
 }
+
